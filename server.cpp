dae1fadb826824f6b1b975ab92135fdd7ab053fb
Inna Yashna <inna.miasoid@gmail.com>
2023-09-13 16:00:32 +0300

    Set up a basic socket server

---
+++ server.cpp
@@ -0,0 +1,18 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(int argc, char const *argv[])
+{
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in server_adress;
+    server_adress.sin_family = AF_INET;
+    server_adress.sin_port = htons(7300);
+    server_adress.sin_addr.s_addr = INADDR_ANY;
+    
+    bind(server_socket, (struct sockaddr*)&server_adress, sizeof(server_adress));
+
+    listen(server_socket, 5);
+
+    int client_socket = accept(server_socket, nullptr, nullptr);
+}
